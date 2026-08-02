af7d2eedf81565375507402e257741a1cc500628
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-08-02 23:15:34 +0600

    Created Category, Product and Rating Models. also added function for rating count.

---
+++ shop/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators   import MinValueValidator, MaxValueValidator
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories' #defined the plural from using verbose name
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=300, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=1)
+    available = models.BooleanField(default=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d')
+
+    def __str__(self):
+        return self.name
+
+    def avarage_rating(self):
+        ratings = self.ratings.all()
+        if ratings.count() > 0:
+            return sum([rating.rating for rating in ratings])/ratings.count()
+
+        return 0
+
+
+
+class Rating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="ratings")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_togather = ('product', 'user') #product and user has to be unique for this product
+
+    def __str__(self):
+        return f"{self.user.name} - {self.product.name} - {self.rating}"
+
