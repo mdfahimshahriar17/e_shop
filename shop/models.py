cdaa3919b5269d7a582ceadc2acddcac7ca9933c
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-08-04 00:46:39 +0600

    Created CartItem model and get_cost function also added get_total_cost and get_total_quantiy in Cart model

--- shop/models.py
+++ shop/models.py
@@ -61,3 +61,21 @@ class Cart(models.Model):
 
     def __set__(self):
         return f"Cart for {self.user.username}"
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+    def get_total_quantity(self):
+        return sum(item.quantity for item in self.items.all())
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="item")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} X {self.product.name}"
+
+    def get_cost(self):
+        return self.product.price*self.quantity
